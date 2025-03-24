0944c9e36531402ec3e6497965e1a014ffe01b90
Christopher Kenny <ctk44@cornell.edu>
2025-03-24 13:15:45 -0400

    work on suggestion stuff

--- _extensions/projector/polylux.lua
+++ _extensions/projector/polylux.lua
@@ -1,29 +1,39 @@
-local in_slide = false  -- Track if we're inside a slide
+local in_slide = false
 
 function Header(el)
   local blocks = {}
 
-  -- Close the previous slide if one was open
+  -- Close any open slide
   if in_slide then
     table.insert(blocks, pandoc.RawBlock("typst", "]"))
+    in_slide = false
   end
 
-  -- Start a new slide
-  table.insert(blocks, pandoc.RawBlock("typst", "#polylux-slide["))
-  table.insert(blocks, pandoc.RawBlock("typst", "= " .. pandoc.utils.stringify(el)))
-
-  in_slide = true  -- We are now inside a slide
-  return blocks
+  if el.level == 1 then
+    -- Register a section
+    local title = pandoc.utils.stringify(el)
+    -- TODO 0.13 update to #toolbox.register-section later
+    table.insert(blocks, pandoc.RawBlock("typst", '#utils.register-section("' .. title .. '")'))
+    return blocks
+  elseif el.level == 2 then
+    -- Start a new slide
+    local title = pandoc.utils.stringify(el)
+    -- TODO 0.13 update to #slide
+    table.insert(blocks, pandoc.RawBlock("typst", "#polylux-slide["))
+    table.insert(blocks, pandoc.RawBlock("typst", "= " .. title))
+    in_slide = true
+    return blocks
+  else
+    -- Lower-level headers stay as-is
+    return el
+  end
 end
 
 function finalize(doc)
   local blocks = doc.blocks
-
-  -- Close the last slide if still open
   if in_slide then
     table.insert(blocks, pandoc.RawBlock("typst", "]"))
   end
-
   return pandoc.Pandoc(blocks, doc.meta)
 end
 
